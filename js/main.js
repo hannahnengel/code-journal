7d224af7213d90e938aa39ca3e5bd23df17cc439
Hannah Engelhardt <hannahnengel@gmail.com>
2021-11-30 21:33:46 -0800

    Listen for 'submit' events on the journal entry form

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /* global data */
 /* exported data */
 
-var $photoURLInput = document.querySelector('#photo-url');
+var $photoURLInput = document.querySelector('#url');
 var $displayedImage = document.querySelector('.displayed-image');
 
 $photoURLInput.addEventListener('input', showPhotoPreview);
@@ -20,4 +20,20 @@ function showPhotoPreview(event) {
   if (isURL(urlLink) !== false) {
     $displayedImage.setAttribute('src', urlLink);
   }
+}
+
+var $form = document.querySelector('form');
+$form.addEventListener('submit', handleInputs);
+function handleInputs(event) {
+  event.preventDefault();
+  var objectOfValues = {
+    title: $form.elements.title.value,
+    url: $form.elements.url.value,
+    notes: $form.elements.notes.value
+  };
+  objectOfValues.nextEntryID = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.push(objectOfValues);
+  $displayedImage.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
 }
