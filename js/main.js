02604dcce4547f936f17a121dd1c1b9029ee069e
Hannah Engelhardt <hannahnengel@gmail.com>
2021-12-09 19:16:00 -0800

    Updated functions per the feedback and added a ul in the html to clean up

--- js/main.js
+++ js/main.js
@@ -27,6 +27,7 @@ var $dataViewEntries = document.querySelector('[data-view="entries"]');
 var $newEntryButton = document.querySelector('.new-entry-button');
 var $form = document.querySelector('form');
 var $entriesLink = document.querySelector('.entries-link');
+var $ulListEntries = document.querySelector('ul.list-entries');
 
 $form.addEventListener('submit', handleInputs);
 function handleInputs(event) {
@@ -45,48 +46,23 @@ function handleInputs(event) {
   $dataViewEntries.classList.remove('hidden');
   $dataViewEntryForm.classList.add('hidden');
   data.view = 'entries';
-  location.reload();
+
+  $ulListEntries.prepend(renderEntries(objectOfValues));
 }
 
 window.addEventListener('DOMContentLoaded', addAnEntry);
 function addAnEntry(entry) {
-  var ulListEntries = document.createElement('ul');
-  ulListEntries.setAttribute('class', 'list-entries');
   if (data.entries.length > 0) {
     for (var i = 0; i < data.entries.length; i++) {
-      var liRowListItem = document.createElement('li');
-      liRowListItem.setAttribute('class', 'row list-item');
-
-      var div1ColumnHalf = document.createElement('div');
-      div1ColumnHalf.setAttribute('class', 'column-half');
-
-      var imgDisplayedImage = document.createElement('img');
-      imgDisplayedImage.setAttribute('src', data.entries[i].url);
-      imgDisplayedImage.setAttribute('alt', data.entries[i].title);
-      imgDisplayedImage.setAttribute('class', 'displayed-image');
-
-      var div2ColumnHalf = document.createElement('div');
-      div2ColumnHalf.setAttribute('class', 'column-half');
-
-      var h1 = document.createElement('h1');
-      h1.textContent = data.entries[i].title;
-
-      var p = document.createElement('p');
-      p.textContent = data.entries[i].notes;
-
-      ulListEntries.appendChild(liRowListItem);
-      liRowListItem.appendChild(div1ColumnHalf);
-      div1ColumnHalf.appendChild(imgDisplayedImage);
-      liRowListItem.appendChild(div2ColumnHalf);
-      div2ColumnHalf.appendChild(h1);
-      div2ColumnHalf.appendChild(p);
+      var newEntry = renderEntries(data.entries[i]);
+      $ulListEntries.prepend(newEntry);
     }
-    $dataViewEntries.appendChild(ulListEntries);
+
   } else {
     var pNoEntries = document.createElement('p');
     pNoEntries.textContent = 'No entries have been recorded.';
     pNoEntries.setAttribute('class', 'center-text');
-    $dataViewEntries.appendChild(pNoEntries);
+    $ulListEntries.prepend(pNoEntries);
   }
   if (data.view === 'entry-form') {
     $dataViewEntries.classList.add('hidden');
@@ -96,6 +72,36 @@ function addAnEntry(entry) {
     $dataViewEntryForm.classList.add('hidden');
   }
   return $dataViewEntries;
+}
+
+function renderEntries(entry) {
+  var liRowListItem = document.createElement('li');
+  liRowListItem.setAttribute('class', 'row list-item');
+
+  var div1ColumnHalf = document.createElement('div');
+  div1ColumnHalf.setAttribute('class', 'column-half');
+
+  var imgDisplayedImage = document.createElement('img');
+  imgDisplayedImage.setAttribute('src', entry.url);
+  imgDisplayedImage.setAttribute('alt', entry.title);
+  imgDisplayedImage.setAttribute('class', 'displayed-image');
+
+  var div2ColumnHalf = document.createElement('div');
+  div2ColumnHalf.setAttribute('class', 'column-half');
+
+  var h1 = document.createElement('h1');
+  h1.textContent = entry.title;
+
+  var p = document.createElement('p');
+  p.textContent = entry.notes;
+
+  liRowListItem.appendChild(div1ColumnHalf);
+  div1ColumnHalf.appendChild(imgDisplayedImage);
+  liRowListItem.appendChild(div2ColumnHalf);
+  div2ColumnHalf.appendChild(h1);
+  div2ColumnHalf.appendChild(p);
+
+  return liRowListItem;
 }
 
 $newEntryButton.addEventListener('click', function (event) {
